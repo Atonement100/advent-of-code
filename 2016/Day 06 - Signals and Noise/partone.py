f26d9651ebf4df981d6da8ccb2799e5b9ef720eb
Timothy Russell-Wagner <t.wagner@ufl.edu>
2016-12-06 00:15:24 -0500

    Day 06 part 1 and 2

---
+++ 2016/Day 06 - Signals and Noise/partone.py
@@ -0,0 +1,23 @@
+inFile = open('input.txt','r')
+instruction_list = [inLine.rstrip('\n') for inLine in inFile]
+
+length = 8
+
+matrix = [[0 for x in range(26)] for y in range(length)]
+
+for instr in instruction_list:
+	for c in range(length):
+		matrix[c][ord(instr[c]) - ord('a')] += 1
+
+out = []
+for y in range(length):
+	maxIndex = 0
+	max = 0
+	for x in range(26):
+		if matrix[y][x] > max:
+			max = matrix[y][x]
+			maxIndex = x
+	out.append(chr(maxIndex + ord('a')))
+print(''.join(out))
+	
+input("Press Enter to continue...")
